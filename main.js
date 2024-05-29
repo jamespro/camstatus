b0f47654e5ed0e067b6e0433bffca0fb937a688a
James Prickitt <james@precision-websites.com>
2024-05-28 22:59:19 -0400

    v01

---
+++ main.js
@@ -0,0 +1,13 @@
+// data: list of approved plates
+// needs to be in a format we can match / compare to, from a string. let's use an array
+approved_plates = ["GHK234", "537VSY", "HOTMIX", "98765432"];
+// future: fetch from file, fetch from remote server
+
+// on click button id plateSubmit,
+//
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>innerHTML</h1>
+  </div>
+`;
