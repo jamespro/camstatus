65144819f671ba20f8e8db6b01b88ef4985bedc9
James Prickitt <james@precision-websites.com>
2024-06-02 12:36:17 -0400

    Fix for schema.ts, Drizzle, create user: set default id
    ``The issue occurs when logging with a new user with oAuth by discord, no user_id is created by drizzle ORM. where as Prisma supports CUID.  as Drizzle requires not-null value without creating one on account creation. current solution i can think of is to assign a default value to user id.```. Here is an update I made to "schema.ts" for SQLite, which it is currently using: ```//for SQLite:
    export const users = createTable("user", {
      id: varchar("id", { length: 255 })
        .notNull()
        .primaryKey()
        .$defaultFn(() => crypto.randomUUID())
    }
    ```

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -32,11 +32,14 @@ export const posts = createTable(
   (example) => ({
     createdByIdIdx: index("createdById_idx").on(example.createdById),
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
 
 export const users = createTable("user", {
-  id: text("id", { length: 255 }).notNull().primaryKey(),
+  id: text("id", { length: 255 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
   name: text("name", { length: 255 }),
   email: text("email", { length: 255 }).notNull(),
   emailVerified: int("emailVerified", {
@@ -73,7 +76,7 @@ export const accounts = createTable(
       columns: [account.provider, account.providerAccountId],
     }),
     userIdIdx: index("account_userId_idx").on(account.userId),
-  })
+  }),
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -91,7 +94,7 @@ export const sessions = createTable(
   },
   (session) => ({
     userIdIdx: index("session_userId_idx").on(session.userId),
-  })
+  }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
@@ -107,5 +110,5 @@ export const verificationTokens = createTable(
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  }),
 );
